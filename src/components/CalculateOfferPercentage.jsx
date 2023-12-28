9707429b1fd86c1c137663bafd0dc3ba086bf9c4
Un-Knwn-me <raghul9510@gmail.com>
2023-12-28 12:58:46 +0530

    commit 3.3

--- src/components/CalculateOfferPercentage.jsx
+++ src/components/CalculateOfferPercentage.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 const CalculateOfferPercentage = ({originalPrice, discountedPrice}) => {
-    const offerPercentage = ((originalPrice - discountedPrice) / originalPrice) * 100;
+    const offerPercentage = Math.round(((originalPrice - discountedPrice) / originalPrice) * 100);
     return offerPercentage;
 }
 
-export default CalculateOfferPercentage
+export default CalculateOfferPercentage;
